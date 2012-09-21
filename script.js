a47a43fc923e53a223e49d349d29863a2c24d69c
Goran Gajic <fero.null@gmail.com>
2012-09-21 10:47:25 +0200

    fixed

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-(function (d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "http://radnicki13.com/fb-test.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'emoticons'));
+function loadScript() {
+  console.log('hi');
+  a = setTimeout(function() {
+    if(document.readyState == "complete") {
+      console.log('completed');
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        console.log(d);
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://raw.github.com/feroc1ty/facebook-emoticons/master/emoticons.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'emoticons'));
+    } else {
+      console.log('notcompleted', a);
+      loadScript();
+    }
+  }, 3000);
+}
+loadScript();
